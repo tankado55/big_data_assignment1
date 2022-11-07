b9e67053ac4a50f8333ba46ee49978d648fdbb37
sara9 <sara9@MSISARA>
2022-11-07 11:57:49 +0100

    Refactoring, added comments

--- batch_sql_extractor.py
+++ batch_sql_extractor.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
-from intercompany_data import IntercompanyData
+from model.intercompany_data import IntercompanyData
 
 
-# Get the stream of NEW DATA (from last month) from OneStrem
-# and aggregate it in the form of intercompany_data and then
-# put them in the historical DB
-# the methods are abstract because the module that provides
-# the data stream can change
+# This class is used to get the stream of new data from the streaming service
+# and aggregate it in the form of intercompany_data to and then
+# put them in the historical DB.
+# The methods are abstract because the module that provides
+# the data stream can change.
 class BatchSQLExtractor(ABC):
     # Get the stream of NEW DATA (from last month) from the DB and
     # aggregate it in the form of intercompany_data
@@ -14,6 +14,7 @@ class BatchSQLExtractor(ABC):
     def aggregate_from_stream(self, stream_conn, query) -> IntercompanyData:
         pass
 
+    # Save the newly generated intercompany_data to the Historical DB.
     @abstractmethod
     def save_intercompany_data(self, hist_db_conn, intercompany_data: IntercompanyData):
         pass
--- main.py
+++ main.py
@@ -2,19 +2,15 @@
 # from batch_sql_extractor import OnestreamBatchSQLExtractor
 # from data_visualizer import GrafanaDataVisualizer
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def main():
-    # with OnestreamConnection.get_conn(server, db_name, credential) as conn:
-    #   new_data = OnestreamBatchSQLExtractor.aggregate_from_stream(conn, query)
-    #   OnestreamBatchSQLExtractor.save_intercompany_data(hist_db_conn, new_data)
+    with OnestreamConnection.get_conn(server, db_name, credential) as conn:
+      new_data = OnestreamBatchSQLExtractor.aggregate_from_stream(conn, query)
+      OnestreamBatchSQLExtractor.save_intercompany_data(hist_db_conn, new_data)
 
-    # intercompany_data = GrafanaDataVisualizer.retrieve_data(hist_db_conn, query)
-    # GrafanaDataVisualizer.draw_data(intercompany_data, visualization_parameters)
+    # intercompany_data = AnalyticsProducer.get_analytics()
+    # A 3rd party application will  be responsible to use our API to retrieve
+    # the analitycs of the intercompany data and then draw them.
 
-    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
